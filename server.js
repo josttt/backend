33cb61a473b0662cdc2f69d98d5766a12f1344cf
josttt <josttristan.toomis@tptlive.ee>
2024-01-17 15:14:52 +0200

    2 page

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("suuuuuiiiii")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+  console.log('somebody visited');
+})
+
+app.get('/2', (req, res) => {
+    res.sendFile(__dirname + '/page2.html');
+    console.log('somebody visited P2');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+})
