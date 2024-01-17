bd96626199f585584502c92f1930fc7f7414740a
josttt <josttristan.toomis@tptlive.ee>
2024-01-17 15:47:21 +0200

    vingev2rktgltei

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
 
+nunjucks.configure('views', {
+    autoescape: true;
+    express: app;
+})
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index.html');
   console.log('somebody visited');
 })
 
 app.get('/2', (req, res) => {
-    res.sendFile(__dirname + '/page2.html');
+    res.sendFile(__dirname + '/views/page2.html');
     console.log('somebody visited P2');
 })
 
