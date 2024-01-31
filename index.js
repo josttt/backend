e37f2a8e2da721a691d23843aa2d446e77642400
josttt <josttristan.toomis@tptlive.ee>
2024-01-31 14:34:05 +0200

    circle calc

--- index.js
+++ index.js
@@ -27,12 +27,15 @@ app.get('/3', (req, res) => {
 
 app.get('/4', (req, res) => {
   res.render('circle.njk', req.query);
-})
+});
 
 app.post('/4', (req, res) => {
-  res.json(req.body);
-})
+  let area = Math.PI * req.body.radius * req.body.radius;
+  let circumference = 2 * Math.PI * req.body.radius;
+  let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
+  res.render("circleAnswer.njk", {r: req.body.radius, a: area, c: circumference, v: volume });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
-})
+});
